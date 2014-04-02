fce54b049ece7d2aa24af4bdd45f92a1440c5753
Roy Klein <royk@commercesciences.com>
2014-04-02 13:55:10 +0300

    Scrape first 8 picks of player 1 from a specific draft.

--- src/main/groovy/script/scrape.groovy
+++ src/main/groovy/script/scrape.groovy
@@ -2,6 +2,12 @@ import geb.Browser
 
 
 Browser.drive {
-    go "http://gatherer.wizards.com/magic/draftools/draftviewer.asp?draftid=08_01_2013_1"
-    output = $("h2").text()
+    def pack = 1
+    def player = 1
+    def pick = 1
+    output = "";
+    for (int i=1; i<9; i++) {
+        go "http://gatherer.wizards.com/magic/draftools/draftviewer.asp?draftid=08_01_2013_1&player="+player+"&pack="+pack+"&pick="+i+"&showpick=true"
+        output += $(".pickedcarddiv .cardlink img").@src + "<br/>";
+    }
 }.quit()
