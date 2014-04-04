a8eef5c69d0539c26d701ce81a60c6af02a07793
Roy Klein <roeiklein@gmail.com>
2014-04-04 11:22:37 +0300

    another..

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,7 +15,7 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        setPort(80);
+        setPort(System.getenv("PORT"));
         get(new FreeMarkerRoute("/view") {
             @Override
             public Object handle(Request request, Response response) {
