08e3385fb383f74eb94bceabcbdbcbc76cc68e26
Roy Klein <roeiklein@gmail.com>
2015-05-22 18:47:34 +0300

    Add conf file

---
+++ src/main/java/Conf.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: royk
+ * Date: 5/18/15
+ * Time: 9:52 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Conf {
+    public static String MONGODB_DB = System.getenv("MONGODB_DB");
+    public static String MONGODB_USER = System.getenv("MONGODB_USER");
+    public static char[] MONGODB_PASS = System.getenv("MONGODB_PASS").toCharArray();
+    public static String MONGODB_URL = System.getenv("MONGODB_URL");
+    public static Integer MONGODB_PORT = Integer.parseInt(System.getenv("MONGODB_PORT"));
+}
