2e11bbcd5bd3b18f633d9e8aed2f3743d5b20b8d
Roy Klein <royk@commercesciences.com>
2015-06-07 13:19:05 +0300

    Close game modal on enter

--- src/main/resources/public/js/app/views/draftGameView.js
+++ src/main/resources/public/js/app/views/draftGameView.js
@@ -2,5 +2,12 @@ App.DraftGameView = Ember.View.extend({
     willInsertElement: function() {
         this._super();
         this.get("controller").send("startGame");
-    }
+    },
+	didInsertElement: function() {
+		$(window).keypress(function(e) {
+			if (e.keyCode===13) {
+				$('.modal').modal('hide');
+			}
+		})
+	}
 });
