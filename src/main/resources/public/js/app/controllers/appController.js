212fe49b09ae322e760aada2b457f43aaeba0f6b
Roy Klein <roeiklein@gmail.com>
2015-09-01 07:34:34 +0300

    new content update

--- src/main/resources/public/js/app/controllers/appController.js
+++ src/main/resources/public/js/app/controllers/appController.js
@@ -11,6 +11,7 @@ App.ApplicationController = Ember.Controller.extend({
     showModeToggler: true,
     showNewsPopup: false,
     latestNews: [
+        "Added World Championship 2015 Drafts".
         "Added Pro Tour Origins Drafts",
         "Pick statistics shown in Training mode",
         "Added Training mode"
